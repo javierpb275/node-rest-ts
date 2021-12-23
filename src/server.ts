7420ddd43e68033ec6ca7602af5375bb30b043d1
javierpb275 <javierpb275@gmail.com>
2021-12-24 00:47:58 +0100

    created scripts for running server and compiling and watch for changes. also added middleware helmet.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 
 class Server {
   public app: express.Application;
@@ -10,7 +11,9 @@ class Server {
 
   config() {
     this.app.set("port", process.env.PORT || 3000);
+    //Middlewares
     this.app.use(morgan('dev'));
+    this.app.use(helmet());
   }
 
   routes() {}
